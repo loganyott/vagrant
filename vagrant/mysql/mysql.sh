c9e043ce388c46eeb7011606eb27edde9d46f0d1
Logan Yott <loganyott@gmail.com>
2015-06-01 11:36:29 -0600

    missing deps for mysql and new logic for checking if db exists

--- vagrant/mysql/mysql.sh
+++ vagrant/mysql/mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo apt-get -y install mysql-server mysql-client php5-mysql
+sudo apt-get -y install mysql-server-5.5 mysql-server mysql-client php5-mysql
 
 # Install MySQL
 echo 'mysql-server mysql-server/root_password password root' | sudo debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password root' | sudo debconf-set-selections
 
 # Create Database if it doesn't exist
-if ! mysql -uroot -proot -e 'use project'; then
+if [ -d /var/lib/mysql/project ]; then
   mysqladmin -uroot -proot create project;
 else
   printf "Project database already exists.\n"
