afe43f05d18d7c43e83d7b370ae71524a75452eb
Logan Yott <loganyott@gmail.com>
2015-06-01 11:49:11 -0600

    Had mysql logic backwards

--- vagrant/mysql/mysql.sh
+++ vagrant/mysql/mysql.sh
@@ -7,7 +7,7 @@ echo 'mysql-server mysql-server/root_password password root' | sudo debconf-set-
 echo 'mysql-server mysql-server/root_password_again password root' | sudo debconf-set-selections
 
 # Create Database if it doesn't exist
-if [ -d /var/lib/mysql/project ]; then
+if [ ! -d /var/lib/mysql/project ]; then
   mysqladmin -uroot -proot create project;
 else
   printf "Project database already exists.\n"
