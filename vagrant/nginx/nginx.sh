4d14b7e2f7585f6f8319b2bd94b9a16128127089
Logan Yott <loganyott@gmail.com>
2015-06-30 12:43:15 -0600

    nginx: removing wp-specific conf

--- vagrant/nginx/nginx.sh
+++ vagrant/nginx/nginx.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y nginx
 # Configure Nginx
 sudo rm /etc/nginx/sites-enabled/*
 sudo cp /var/vagrant/nginx/localhost.conf /etc/nginx/sites-enabled/localhost.conf
+sed -i "s:/srv/www:$DOCROOT:" /etc/nginx/sites-enabled/localhost.conf
 sudo cp /var/vagrant/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # Restart Nginx
-sudo service nginx restart
+sudo service nginx reload
