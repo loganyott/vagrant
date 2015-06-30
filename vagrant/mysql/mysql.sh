48317334ce9dba2a7738aace96a37b857afb788a
Logan Yott <loganyott@gmail.com>
2015-06-30 12:40:37 -0600

    mysql: Forcing dep download until I can debug the real issue

--- vagrant/mysql/mysql.sh
+++ vagrant/mysql/mysql.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sudo apt-get -yf install mysql-server mysql-client php5-mysql
+sudo apt-get -y install mysql-server mysql-client php5-mysql
+# TODO figure out why mysql won't resolve deps without this
+sudo apt-get -f install
 
 # Install MySQL
 echo 'mysql-server mysql-server/root_password password root' | sudo debconf-set-selections
