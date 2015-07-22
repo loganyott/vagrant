c8dac061a59f5df0b7a416937839de3ed31b87f1
Logan Yott <loganyott@gmail.com>
2015-07-21 22:12:34 -0600

    nginx: more complete docroot replacement in provisioner

--- vagrant/nginx/nginx.sh
+++ vagrant/nginx/nginx.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y nginx
 # Configure Nginx
 sudo rm /etc/nginx/sites-enabled/*
 sudo cp /var/vagrant/nginx/localhost.conf /etc/nginx/sites-enabled/localhost.conf
-sed -i "s:/srv/www:$DOCROOT:" /etc/nginx/sites-enabled/localhost.conf
+sed -i "s:root .*$:root $DOCROOT;:" /etc/nginx/sites-enabled/localhost.conf
 sudo cp /var/vagrant/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # Restart Nginx
